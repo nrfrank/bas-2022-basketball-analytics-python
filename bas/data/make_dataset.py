fc266964a6038ae77f46c90efce9c22ad3f8ee11
Nathan Frank <nathan.frank@strong.io>
2022-04-14 12:13:40 -0500

    Update exploration notebook to use package methods. Write data generation script.

--- bas/data/make_dataset.py
+++ bas/data/make_dataset.py
@@ -1,30 +1,51 @@
 # -*- coding: utf-8 -*-
-import click
+import argparse
+
 import logging
+import sys
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+
+import pandas as pd
+
+from bas.data.shots import get_game_shots
+
+from bas.data.season import get_season_schedule
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(cli_args):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info('Generating full season shot dataset.')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "season",
+        type=int,
+        help="The season to generate data for."
+    )
+    args = parser.parse_args(cli_args)
+    logger.info(f'Getting schedule for season {args.season}.')
+    season_df = get_season_schedule(args.season)
+
+    logger.info(f'Generating shot data for all games in season {args.season}.')
+    all_shots = []
+    for row in season_df.itertuples():
+        shot_df = get_game_shots(row)
+        all_shots.append(shot_df)
+
+    logger.info('Compiling game level shot data.')
+    all_shots_df = pd.concat(all_shots, ignore_index=True)
+
+    project_dir = Path(__file__).resolve().parents[2]
+    save_path = project_dir / 'data' / f'shot_data_{args.season}.csv'
+    logger.info(f'Writing shot data to {save_path}.')
+    all_shots_df.to_csv(save_path, index=False)
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    main(sys.argv[1:])
--- notebooks/exploration.py
+++ notebooks/exploration.py
@@ -16,94 +16,21 @@
 import pandas as pd
 import seaborn as sns
 
-from basketball_reference_scraper.seasons import get_schedule
-from basketball_reference_scraper.shot_charts import get_shot_chart
+from bas.data.season import get_season_schedule
+from bas.data.shots import get_game_shots
 
 # %%
-TEAM_NAME_TO_ABBREV = {
-    'Atlanta Hawks': 'ATL',
-    'Boston Celtics': 'BOS',
-    'Brooklyn Nets': 'BRK',
-    'Charlotte Bobcats': 'CHA',
-    'Charlotte Hornets': 'CHO',
-    'Chicago Bulls': 'CHI',
-    'Cleveland Cavaliers': 'CLE',
-    'Dallas Mavericks': 'DAL',
-    'Denver Nuggets': 'DEN',
-    'Detroit Pistons': 'DET',
-    'Golden State Warriors': 'GSW',
-    'Houston Rockets': 'HOU',
-    'Indiana Pacers': 'IND',
-    'Los Angeles Clippers': 'LAC',
-    'Los Angeles Lakers': 'LAL',
-    'Memphis Grizzlies': 'MEM',
-    'Miami Heat': 'MIA',
-    'Milwaukee Bucks': 'MIL',
-    'Minnesota Timberwolves': 'MIN',
-    'New Jersey Nets': 'NJN',
-    'New Orleans Hornets': 'NOH',
-    'New Orleans Pelicans': 'NOP',
-    'New Orleans/Oklahoma City Hornets': 'NOK',
-    'New York Knicks': 'NYK',
-    'Oklahoma City Thunder': 'OKC',
-    'Orlando Magic': 'ORL',
-    'Philadelphia 76ers': 'PHI',
-    'Phoenix Suns': 'PHO',
-    'Portland Trail Blazers': 'POR',
-    'Sacramento Kings': 'SAC',
-    'San Antonio Spurs': 'SAS',
-    'Seattle SuperSonics': 'SEA',
-    'Toronto Raptors': 'TOR',
-    'Utah Jazz': 'UTA',
-    'Vancouver Grizzlies': 'VAN',
-    'Washington Wizards': 'WAS'
-}
+season_df = get_season_schedule(2021)
 
 # %%
-sdf = get_schedule(2021, playoffs=False)
-sdf['HOME_ABBREV'] = sdf['HOME'].map(TEAM_NAME_TO_ABBREV)
-sdf['VISITOR_ABBREV'] = sdf['VISITOR'].map(TEAM_NAME_TO_ABBREV)
+print(season_df.shape)
+season_df.sample(15)
 
 # %%
-sdf.sample(15)
+sample_game = season_df.sample(1).iloc[0]
 
 # %%
-sample_game = sdf.sample(1).iloc[0]
-
-# %%
-sample_game
-
-# %%
-for row in sdf.itertuples():
-    print(row.DATE)
-
-# %%
-type(row)
-
-
-# %%
-
-# %%
-def get_game_shots(game) -> pd.DataFrame:
-    shots = get_shot_chart(game.DATE, game.HOME_ABBREV, game.VISITOR_ABBREV)
-    shot_df_home = process_shot_df(shots[game.HOME_ABBREV], game.HOME_ABBREV, game.DATE)
-    shot_df_away = process_shot_df(shots[game.VISITOR_ABBREV], game.VISITOR_ABBREV, game.DATE)
-    return pd.concat([shot_df_home, shot_df_away], ignore_index=True)
-
-
-def process_shot_df(shot_df: pd.DataFrame, team: str, date: str) -> pd.DataFrame:
-    shot_df.columns = [c.lower() for c in shot_df.columns]
-    shot_df['x'] = shot_df['x'].str.replace(' ft', '').astype(float)
-    shot_df['y'] = shot_df['y'].str.replace(' ft', '').astype(float)
-    shot_df['distance'] = shot_df['distance'].str.replace(' ft', '').astype(float)
-    shot_df['label'] = shot_df['make_miss'].map({'MAKE': 1, 'MISS': 0})
-    shot_df['team'] = team
-    shot_df['date'] = date
-    return shot_df
-
-
-# %%
-shot_df = get_game_shots(row)
+shot_df = get_game_shots(sample_game)
 
 # %%
 g = sns.relplot(
@@ -114,6 +41,10 @@ g = sns.relplot(
     data=shot_df, 
     palette=dict(MAKE=sns.color_palette()[2], MISS=sns.color_palette()[3])
 )
-g.set(xlim=(0, 50), ylim=(0, 42))
+g.set(
+    xlim=(0, 50), 
+    ylim=(0, 42)
+)
+g.fig.suptitle(f"{sample_game.HOME_ABBREV} v {sample_game.VISITOR_ABBREV} - {sample_game.DATE.date()}", y=1.05)
 
 # %%
